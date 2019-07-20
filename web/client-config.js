6a7158ecee2173528dc3054e329141ade3b7dc8a
Jamie Bradley <jamie@endeavour-digital.com>
2019-07-20 21:38:27 +0100

    Backing up local changes

--- web/client-config.js
+++ web/client-config.js
@@ -1,6 +1,8 @@
+console.log("This is a test", process.env.GATSBY_SANITY_DATASET);
+
 module.exports = {
   sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'ghm8pz5f',
-    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || "ghm8pz5f",
+    dataset: process.env.GATSBY_SANITY_DATASET || "production"
   }
-}
+};
--- web/src/containers/layout.js
+++ web/src/containers/layout.js
@@ -1,44 +1,46 @@
-import {graphql, StaticQuery} from 'gatsby'
-import React, {useState} from 'react'
-import Layout from '../components/layout'
+import { graphql, StaticQuery } from "gatsby";
+import React, { useState } from "react";
+import Layout from "../components/layout";
 
 const query = graphql`
   query SiteTitleQuery {
-    site: sanitySiteSettings(_id: {regex: "/(drafts.|)siteSettings/"}) {
+    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
       title
     }
   }
-`
+`;
 
-function LayoutContainer (props) {
-  const [showNav, setShowNav] = useState(false)
-  function handleShowNav () {
-    setShowNav(true)
+function LayoutContainer(props) {
+  const [showNav, setShowNav] = useState(false);
+  function handleShowNav() {
+    setShowNav(true);
   }
-  function handleHideNav () {
-    setShowNav(false)
+  function handleHideNav() {
+    setShowNav(false);
   }
   return (
-    <StaticQuery
-      query={query}
-      render={data => {
-        if (!data.site) {
-          throw new Error(
-            'Missing "Site settings". Open the Studio at http://localhost:3333 and some content in "Site settings"'
-          )
-        }
-        return (
-          <Layout
-            {...props}
-            showNav={showNav}
-            siteTitle={data.site.title}
-            onHideNav={handleHideNav}
-            onShowNav={handleShowNav}
-          />
-        )
-      }}
-    />
-  )
+    // <StaticQuery
+    //   query={query}
+    //   render={data => {
+    //     if (!data.site) {
+    //       console.log("data", data);
+    //       throw new Error(
+    //         'Missing "Site settings". Open the Studio at http://localhost:3333 and some content in "Site settings"'
+    //       );
+    //     }
+    //     return (
+    //       <Layout
+    //         {...props}
+    //         showNav={showNav}
+    //         siteTitle={data.site.title}
+    //         onHideNav={handleHideNav}
+    //         onShowNav={handleShowNav}
+    //       />
+    //     );
+    //   }}
+    // />
+    <h1>Hello</h1>
+  );
 }
 
-export default LayoutContainer
+export default LayoutContainer;
